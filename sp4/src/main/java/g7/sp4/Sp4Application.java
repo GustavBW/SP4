57b8e0c855cd5335890b30d0d9d0c3db45ddbf5b
Kemoe19 <kemoe19@student.sdu.dk>
2023-03-23 13:17:13 +0100

    java

---
+++ sp4/src/main/java/g7/sp4/Sp4Application.java
@@ -0,0 +1,13 @@
+package g7.sp4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp4Application.class, args);
+	}
+
+}
