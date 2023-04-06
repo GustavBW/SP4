e6f36f916fddf2b9aa94a80c2cc617334d4e6c4c
BJLUN21 <bjlun21@student.sdu.dk>
2023-04-06 10:11:12 +0200

    MQTT-halløj

--- backend/src/main/java/g7/sp4/protocolHandling/AssmConnectionService.java
+++ backend/src/main/java/g7/sp4/protocolHandling/AssmConnectionService.java
@@ -1,4 +1,10 @@
 package g7.sp4.protocolHandling;
 
+import g7.sp4.common.models.AssmStatus;
+
 public interface AssmConnectionService {
+
+	AssmStatus getStatus();
+	Flag build();
 }
+
--- backend/src/main/java/g7/sp4/protocolHandling/AssmConnector.java
+++ backend/src/main/java/g7/sp4/protocolHandling/AssmConnector.java
@@ -1,4 +1,60 @@
 package g7.sp4.protocolHandling;
 
-public class AssmConnector {
+import g7.sp4.common.models.AssmStatus;
+import g7.sp4.util.JSONWrapper;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+public class AssmConnector implements AssmConnectionService {
+
+	String broker = "mqtt://localhost:1883/";
+	String clientId = "subscribe_client";
+
+	MqttClient client;
+
+	{
+		try {
+			client = new MqttClient(broker, clientId, new MemoryPersistence());
+			client.connect();
+		} catch (MqttException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public AssmStatus getStatus() {
+		String bodyAsString;
+		int code = 200;
+
+		String msg;
+
+		try {
+
+			String status = client.subscribeWithResponse("emulator/status").toString();
+			JSONWrapper wrapped = new JSONWrapper(status);
+			String processID = wrapped.get("CurrentOperation");
+			int state = Integer.parseInt(wrapped.get("State"));
+
+			if (state == 0) {
+				msg = "Idle";
+			} else if (state == 1) {
+				msg = "Executing";
+			} else msg = "Error";
+
+			return new AssmStatus(
+					processID,
+					msg,
+					state
+			);
+		} catch (MqttException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	@Override
+	public Flag build() {
+		return null;
+	}
 }
