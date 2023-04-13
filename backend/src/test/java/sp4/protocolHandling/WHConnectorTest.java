0d32433f36ed86d83d0a952b85eb960c1c58656b
Kemoe19 <kemoe19@student.sdu.dk>
2023-04-13 10:29:26 +0200

    whconnectorTEst

---
+++ backend/src/test/java/sp4/protocolHandling/WHConnectorTest.java
@@ -0,0 +1,20 @@
+package java.sp4.protocolHandling;
+
+import g7.sp4.common.models.AGVState;
+import g7.sp4.common.models.AGVStatus;
+import g7.sp4.protocolHandling.AGVConnector;
+import g7.sp4.protocolHandling.Flag;
+import org.junit.jupiter.api.*;
+
+import java.sql.SQLOutput;
+import java.util.function.Function;
+
+public class WHConnectorTest {
+
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("||| Testing WHConnector ================== Testing WHConnector |||");
+    }
+
+
+}
