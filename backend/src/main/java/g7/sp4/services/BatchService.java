e34ecc84ec5ddea0785195fdec85deb5ab7b2d00
GustavBW <gustavbw@gmail.com>
2023-04-28 10:51:02 +0200

    Recipe check on batch commit

--- backend/src/main/java/g7/sp4/services/BatchService.java
+++ backend/src/main/java/g7/sp4/services/BatchService.java
@@ -2,7 +2,10 @@ package g7.sp4.services;
 
 import g7.sp4.common.models.Batch;
 import g7.sp4.common.models.BatchPart;
+import g7.sp4.common.models.Part;
+import g7.sp4.repositories.RecipeRepository;
 import g7.sp4.util.responseUtil.BatchResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -10,12 +13,22 @@ import java.util.Map;
 
 @Service
 public class BatchService implements IBatchService{
+
+    @Autowired
+    private RecipeRepository recipeRepo;
+
     @Override
     public String verify(Batch batch) {
 
         if(batch == null || batch.isHasCompleted()) return "invalid batch";
         if(batch.getParts().isEmpty()) return "empty batch";
         if(batch.getEmployeeId().isBlank()) return "please provide id";
+
+        for(BatchPart part : batch.getParts()){
+            if(recipeRepo.findById(part.getPartId()).orElse(null) == null){
+                return "No recipe available for part id: " + part.getPartId();
+            }
+        }
 
         return null;
     }
