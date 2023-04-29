caa78f57077555be066b56a60dd34c69c910dc01
GustavBW <37631401+GustavBW@users.noreply.github.com>
2023-04-30 00:53:59 +0200

    Documentation Updates & Service Upgrades
    Updated Batch DTO to include more information on each part included in said batch

--- backend/src/main/java/g7/sp4/util/responseUtil/BatchResponse.java
+++ backend/src/main/java/g7/sp4/util/responseUtil/BatchResponse.java
@@ -2,5 +2,5 @@ package g7.sp4.util.responseUtil;
 
 import java.util.Map;
 
-public record BatchResponse(long id, boolean hasCompleted, String employee, Map<Long, Integer> partsCountMap) {
+public record BatchResponse(long id, boolean hasCompleted, String employee, Map<PartResponse, Integer> partsCountMap) {
 }
