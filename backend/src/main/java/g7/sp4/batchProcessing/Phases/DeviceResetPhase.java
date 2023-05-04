37bc60b0f22adaad00ac14c6ef05c5a24140eabe
GustavBW <37631401+GustavBW@users.noreply.github.com>
2023-05-04 20:02:40 +0200

    Device Reset Phase

--- backend/src/main/java/g7/sp4/batchProcessing/Phases/DeviceResetPhase.java
+++ backend/src/main/java/g7/sp4/batchProcessing/Phases/DeviceResetPhase.java
@@ -1,11 +1,91 @@
 package g7.sp4.batchProcessing.Phases;
 
+import g7.sp4.common.models.AssmState;
 import g7.sp4.common.models.Batch;
 import g7.sp4.common.models.BatchPart;
+import g7.sp4.common.models.WHState;
+import g7.sp4.protocolHandling.AGVConnectionService;
+import g7.sp4.protocolHandling.AssmConnectionService;
+import g7.sp4.protocolHandling.Flag;
+import g7.sp4.protocolHandling.WHConnectionService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class DeviceResetPhase extends Phase{
+    //As per documentation, the DeviceResetPhase, executed by the DeviceResetService, returns all devices to some "original" position.
+
+    private int stateTracker = 0;
+
+    private long timeStartWH = 0;
+    private boolean awaitWHIdleHasBegun = false;
+    private final int maxAllowedWHTimeoutSeconds = 15;
+
+    private long timeStartAssm = 0;
+    private boolean awaitAssmHasBegun = false;
+    private final int maxAllowedAssmTimeoutSeconds = 15;
+
+    private Flag moveAGVToWHFlag;
+
+    @Autowired
+    private AGVConnectionService agvConnector;
+    @Autowired
+    private WHConnectionService whConnector;
+
+    @Autowired
+    private AssmConnectionService assmConnector;
+
+
     @Override
     public PhaseUpdateResult update(Batch batch, BatchPart currentPart) {
-        return null;
+        switch (stateTracker) {
+            case 0 -> { // reset the agv to be at the warehouse
+                if(moveAGVToWHFlag == null){
+                    moveAGVToWHFlag = agvConnector.moveToWarehouse();
+                }
+
+                if(moveAGVToWHFlag.get()){ //the agv is now at the warehouse.
+                    stateTracker++;
+                }
+
+                if(moveAGVToWHFlag.hasError()){
+                    //some fatal error that cant be automatically resolved
+                    return new PhaseUpdateResult(false, true);
+                }
+            }
+            case 1 -> { //assure that the wh is idle
+                if(!awaitWHIdleHasBegun){
+                    timeStartWH = System.currentTimeMillis();
+                    awaitWHIdleHasBegun = true;
+                }
+
+                if (whConnector.getStatus().whState() == WHState.IDLE){
+                    stateTracker++;
+                }
+                //accounting for timeouts, however allowing some time for the connection to reestablish
+                if(timeStartWH + maxAllowedWHTimeoutSeconds < System.currentTimeMillis()){
+                    //this has taken too long, there is an error somewhere
+                    return new PhaseUpdateResult(false, true);
+                }
+            }
+            case 2 -> { // assure that the Assembler is idle
+                if(!awaitAssmHasBegun){
+                    awaitAssmHasBegun = true;
+                    timeStartAssm = System.currentTimeMillis();
+                }
+
+                if(assmConnector.getStatus().state() == AssmState.IDLE){
+                    stateTracker++;
+                }
+                //accounting for timeouts, however allowing some time for the connection to reestablish
+                if(timeStartAssm + maxAllowedAssmTimeoutSeconds < System.currentTimeMillis()){
+                    return new PhaseUpdateResult(false, true);
+                }
+            }
+            case 3 -> { //and we're done
+                return new PhaseUpdateResult(true, false);
+            }
+
+        }
+
+        return new PhaseUpdateResult(false, false);
     }
 }
