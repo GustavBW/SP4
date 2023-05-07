94518deafd26607b00e01f23b904223532541d03
mShadow56 <72358834+mShadow56@users.noreply.github.com>
2023-05-07 21:57:09 +0200

    Phase 3 og 6

--- backend/src/main/java/g7/sp4/batchProcessing/Phases/AGVReturnToWHPhase.java
+++ backend/src/main/java/g7/sp4/batchProcessing/Phases/AGVReturnToWHPhase.java
@@ -2,11 +2,71 @@ package g7.sp4.batchProcessing.Phases;
 
 import g7.sp4.common.models.Batch;
 import g7.sp4.common.models.BatchPart;
+import g7.sp4.protocolHandling.AGVConnectionService;
+import g7.sp4.protocolHandling.Flag;
+import g7.sp4.services.IEventLoggingService;
+import org.springframework.beans.factory.annotation.Autowired;
 
+//kenneth og nikolaj
 public class AGVReturnToWHPhase extends Phase{
+
+    private int stateTracker = 0;
+
+    private Flag agvMovingToWHFLag;
+
+    @Autowired
+    private AGVConnectionService agvConnector;
+
+    @Autowired
+    private IEventLoggingService eventService;
+
     @Override
     public PhaseUpdateResult update(Batch batch, BatchPart currentPart) {
-        return null;
-    }
 
+    switch (stateTracker) {
+        case 0 -> {
+            // Moving the agv to the warehouse
+            if (agvMovingToWHFLag==null){
+
+                eventService.createNewEvent(
+                        batch,
+                        "Moving AGV towards WH",
+                        false,
+                        "The AGV is underway towards the Warehouse."
+                );
+                agvMovingToWHFLag=agvConnector.moveToWarehouse();
+            }
+
+
+            if(agvMovingToWHFLag.get()){
+                stateTracker++;
+            }
+
+            if(agvMovingToWHFLag.hasError()) {
+                throwErrorEvent(agvMovingToWHFLag, batch);
+                return new PhaseUpdateResult(false, true);
+            }
+
+        }
+
+        case 1 -> {
+            //Finish
+            return new PhaseUpdateResult(true, false);
+        }
+
+    }
+        return new PhaseUpdateResult(false, false);
 }
+
+
+    private void throwErrorEvent(Flag flag, Batch batch){
+        eventService.createNewEvent(
+                batch,
+                flag.getError().name(),
+                true,
+                flag.getError().description()
+        );
+    }
+}
+
+
--- backend/src/main/java/g7/sp4/batchProcessing/Phases/LoadAssemblerPhase.java
+++ backend/src/main/java/g7/sp4/batchProcessing/Phases/LoadAssemblerPhase.java
@@ -2,11 +2,89 @@ package g7.sp4.batchProcessing.Phases;
 
 import g7.sp4.common.models.Batch;
 import g7.sp4.common.models.BatchPart;
+import g7.sp4.protocolHandling.AGVConnectionService;
+import g7.sp4.protocolHandling.Flag;
+import g7.sp4.services.IEventLoggingService;
+import org.springframework.beans.factory.annotation.Autowired;
 
+//kenneth og nikolaj
 public class LoadAssemblerPhase extends Phase{
+
+    private int stateTracker = 0;
+
+    private Flag agvAtAssemblerFlag, agvHasPutDownPartFlag;
+
+
+    @Autowired
+    private AGVConnectionService agvConnector;
+
+    @Autowired
+    private IEventLoggingService eventService;
+
     @Override
     public PhaseUpdateResult update(Batch batch, BatchPart currentPart) {
-        return null;
+
+        switch (stateTracker) {
+            case 0 -> {
+                //Check that the AVG have arrived at the assembly
+
+
+               if (agvAtAssemblerFlag==null) {
+                   agvAtAssemblerFlag = agvConnector.moveToAssembly();
+               }
+
+                if(agvAtAssemblerFlag.get()){
+                    stateTracker++;
+                }
+
+                if(agvAtAssemblerFlag.hasError()) {
+                    throwErrorEvent(agvAtAssemblerFlag, batch);
+                    return new PhaseUpdateResult(false, true);
+                }
+
+            }
+            case 1 -> {
+                //make the AGV put the part or component on the assembler
+                if(agvHasPutDownPartFlag == null){
+                    eventService.createNewEvent(
+                            batch,
+                            "Placing part at assembly",
+                            false,
+                            "The AGV is currently placing the part at the assembler."
+                    );
+                    agvHasPutDownPartFlag = agvConnector.putItemAtAssembly();
+                }
+
+                if(agvHasPutDownPartFlag.get()){
+                    stateTracker++;
+                }
+
+                if(agvHasPutDownPartFlag.hasError()) {
+                    throwErrorEvent(agvHasPutDownPartFlag, batch);
+                    return new PhaseUpdateResult(false, true);
+                }
+
+            }
+            case 2 -> {
+                //Finish
+                return new PhaseUpdateResult(true, false);
+            }
+
+        }
+        return new PhaseUpdateResult(false, false);
     }
+
+
+    private void throwErrorEvent(Flag flag, Batch batch){
+        eventService.createNewEvent(
+                batch,
+                flag.getError().name(),
+                true,
+                flag.getError().description()
+        );
+    }
+
+
+
 }
 
