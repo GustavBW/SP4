8d54a1c38934f96dd05e561224af8adde0e78c69
Dlov507 <dlo.sy@outlook.com>
2023-05-22 10:22:11 +0200

    rest of phases impl

--- backend/src/main/java/g7/sp4/batchProcessing/Phases/AGVGoChargePhase.java
+++ backend/src/main/java/g7/sp4/batchProcessing/Phases/AGVGoChargePhase.java
@@ -2,10 +2,44 @@ package g7.sp4.batchProcessing.Phases;
 
 import g7.sp4.common.models.Batch;
 import g7.sp4.common.models.BatchPart;
+import g7.sp4.protocolHandling.Flag;
 
 public class AGVGoChargePhase extends Phase{
+    private int stateTracker = 0;
+    private Flag agvGoChargeFlag;
+
     @Override
     public PhaseUpdateResult update(Batch batch, BatchPart currentPart) {
-        return new PhaseUpdateResult(true, false);
+
+        switch (stateTracker) {
+            case 0 -> {
+                if(agvGoChargeFlag == null) {
+                    eventService.createNewEvent(
+                            batch,
+                            "AGV going to charge",
+                            false,
+                            "The AGV is on way to charging station."
+                    );
+                    agvGoChargeFlag = agvConnector.moveToCharger();
+                }
+
+                if(agvGoChargeFlag.get()){
+                    stateTracker++;
+                }
+
+                if(agvGoChargeFlag.hasError()) {
+                    throwErrorEvent(agvGoChargeFlag, batch);
+                    return new PhaseUpdateResult(false, true);
+                }
+            }
+            case 1 -> {
+                return new PhaseUpdateResult(true, false);
+            }
+        }
+        return PhaseUpdateResult.FALSE_FALSE;
     }
-}
+
+
+//        return new PhaseUpdateResult(true, false);
+    }
+
--- backend/src/main/java/g7/sp4/batchProcessing/Phases/TransportToAssemblerPhase.java
+++ backend/src/main/java/g7/sp4/batchProcessing/Phases/TransportToAssemblerPhase.java
@@ -2,10 +2,41 @@ package g7.sp4.batchProcessing.Phases;
 
 import g7.sp4.common.models.Batch;
 import g7.sp4.common.models.BatchPart;
+import g7.sp4.protocolHandling.Flag;
 
 public class TransportToAssemblerPhase extends Phase{
+    private int stateTracker = 0;
+    private Flag agvMoveToAssemblerFlag;
+
     @Override
     public PhaseUpdateResult update(Batch batch, BatchPart currentPart) {
-        return new PhaseUpdateResult(true, false);
+        switch (stateTracker) {
+            case 0 -> {
+                if(agvMoveToAssemblerFlag == null) {
+                    eventService.createNewEvent(
+                            batch,
+                            "AGV moving to Assembler",
+                            false,
+                            "The AGV is on way  towards the assembler."
+                    );
+                    agvMoveToAssemblerFlag = agvConnector.moveToAssembly();
+                }
+
+                if(agvMoveToAssemblerFlag.get()){
+                    stateTracker++;
+                }
+
+                if(agvMoveToAssemblerFlag.hasError()) {
+                    throwErrorEvent(agvMoveToAssemblerFlag, batch);
+                    return new PhaseUpdateResult(false, true);
+                }
+            }
+            case 1 -> {
+
+                return new PhaseUpdateResult(true, false);
+            }
+        }
+        return PhaseUpdateResult.FALSE_FALSE;
     }
+
 }
